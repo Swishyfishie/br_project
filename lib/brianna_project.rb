84666ba0d2c73c9ed5986c7181055deb9ec546f4
Swishyfishie <pclandreiuk@gmail.com>
2020-02-16 22:50:11 +0000

    first commit

---
+++ lib/brianna_project.rb
@@ -0,0 +1,6 @@
+require "brianna_project/version"
+
+module BriannaProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/brianna_project/version.rb
@@ -0,0 +1,3 @@
+module BriannaProject
+  VERSION = "0.1.0"
+end
